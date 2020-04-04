ccd58b74ec9e68f55578fa6dc4cd675e3af55a3d
Pedro <pedro.luis.almeida.victor@protonmail.com>
2020-04-04 19:17:22 -0400

    Homepage with key concepts

---
+++ django/src/accounts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=25)
+    bio = models.CharField(max_length=200)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User)
+
