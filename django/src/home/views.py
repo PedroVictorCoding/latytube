4c57deb28f513fded5af8dd6b64c7242d7624913
Pedro <pedro.luis.almeida.victor@protonmail.com>
2020-04-05 18:52:22 -0400

    All pages with animation; video upload working; Map working;

--- django/src/home/views.py
+++ django/src/home/views.py
@@ -3,7 +3,20 @@ from .models import Video
 from .forms import VideoForm
 
 def homepage(request):
+    consumer_ip = get_client_ip(request)
+    print("Consumer IP: " + consumer_ip)
     return render(request, 'home/homepage.html')
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 
 def map_view(request):
     return render(request, 'home/map_view.html')
@@ -18,6 +31,7 @@ def showvideo(request):
     if form.is_valid():
         video_upload = form.save(commit=False)
         video_upload.author = request.user
+        #video_upload.location = LonlatIPVal
         # TODO: Generate JSON inside media folder
         form.save()
 
