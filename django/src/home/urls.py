15ddbc49a2fc0de67eb176924af9dca3b47b6932
Pedro <pedro.luis.almeida.victor@protonmail.com>
2020-04-05 14:54:19 -0400

    Base map implemented; Video Upload implemented; background animation implemented

--- django/src/home/urls.py
+++ django/src/home/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView
 from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
-from home.views import homepage
+from home.views import homepage, map_view, showvideo
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('map/', map_view, name='map'),
+    path('feed/', showvideo, name="feed")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django/src/home/views.py
+++ django/src/home/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
+from .models import Video
+from .forms import VideoForm
 
 def homepage(request):
     return render(request, 'home/homepage.html')
+
+def map_view(request):
+    return render(request, 'home/map_view.html')
+
+
+
+def showvideo(request):
+
+    lastvideos = Video.objects.all().order_by('-date_of_upload')
+
+    form= VideoForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        video_upload = form.save(commit=False)
+        video_upload.author = request.user
+        # TODO: Generate JSON inside media folder
+        form.save()
+
+    
+    context= {
+        'lastvideos': lastvideos,
+        'form': form
+              }
+    
+      
+    return render(request, 'home/feed.html', context)
