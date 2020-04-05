76eecf9e2ddc1aa28cca04014ce375a5a1c90cb1
Pedro <pedro.luis.almeida.victor@protonmail.com>
2020-04-05 14:54:19 -0400

    Base map implemented; Video Upload implemented; background animation implemented

---
+++ django/src/home/forms.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from home.models import Video
+
+
+class VideoForm(forms.ModelForm):
+    title = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title'}))
+    tags = forms.CharField(required=False, label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Tags'}))
+    videofile = forms.FileField(required=True, label="", widget=forms.FileInput(attrs={'id': 'upload-btn', 'class': 'file-upload', 'type': 'file'}))
+    
+    class Meta:
+        model= Video
+        fields= ["title", "videofile", "tags"]
--- django/src/home/models.py
+++ django/src/home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Video(models.Model):
+    #User Powered
+    title = models.CharField(max_length=50)
+    videofile = models.FileField(upload_to='videos/%Y/%m/%d/', null=True, verbose_name="")
+    tags = models.CharField(max_length=50, blank=True)
+    
+    #Generated
+    location = models.CharField(max_length=500, blank=False)
+    like_count = models.IntegerField(null=True, blank=True)
+    view_count = models.IntegerField(null=True, blank=True)
+    thumbnail = models.ImageField()
+    author = models.CharField(max_length=150)
+    date_of_upload = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title + ": " + str(self.videofile)
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
