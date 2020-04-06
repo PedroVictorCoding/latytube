2c3928879aa126ce8673b66c7eeb65ed7e4ca715
Pedro <pedro.luis.almeida.victor@protonmail.com>
2020-04-06 02:38:48 -0400

    Map markers working; video fullscreen working; and more

--- django/src/home/urls.py
+++ django/src/home/urls.py
@@ -5,10 +5,11 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView
 from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
-from home.views import homepage, map_view, showvideo
+from home.views import homepage, map_view, showvideo, showfullvideo
 
 urlpatterns = [
     path('', homepage, name='homepage'),
     path('map/', map_view, name='map'),
-    path('feed/', showvideo, name="feed")
+    path('feed/', showvideo, name="feed"),
+    path('feed/v/<int:pk>', showfullvideo, name="individualvid")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
