654c12b63281c0d300668aae2214ce9b03e4391b
Pedro Victor <pedro.luis.almeida.victor@protonmail.com>
2020-06-11 00:54:43 +0000

    server settings

--- django/src/src/settings.py
+++ django/src/src/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%n=phqu-31(10)dj=+jm%9=u!xc!be*31od0y@4!42-+2par0('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["www.notatish.com"]
 
