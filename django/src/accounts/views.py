8da9ce48ef481370a1457fdd9696f8c0abf06785
Pedro Victor <pedro.luis.almeida.victor@protonmail.com>
2020-06-10 20:05:32 -0400

    Signup fiz attempt

--- django/src/accounts/views.py
+++ django/src/accounts/views.py
@@ -21,9 +21,12 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            new_user = form.save()
-            login(request, new_user)
-            return HttpResponseRedirect('/')
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect('/map')
     else:
         form = SignupForm()
 
