d176df83737fa9c4b104ff623b30dbb41e6945ba
Pedro Victor <47682329+PedroVictorCoding@users.noreply.github.com>
2021-07-19 16:02:33 -0400

    Update settings.py

--- django/src/src/settings.py
+++ django/src/src/settings.py
@@ -166,7 +166,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'latytube@gmail.com'
-EMAIL_HOST_PASSWORD = 'Emc231415'
+EMAIL_HOST_PASSWORD = '<FILL-IN>'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
